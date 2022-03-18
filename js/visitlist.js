e5ac2315579ce020c1888ce2414946f37cdd8d91
sunil101-coder <chaduarysunil@gmail.com>
2022-03-18 13:22:13 +0530

    visitlist update

--- js/visitlist.js
+++ js/visitlist.js
@@ -1,39 +1,39 @@
 const vist = [{
         id: 1,
-        image: "http://localhost/BlueValley/img/visit1.jpg",
+        image: "http://localhost/BlueValley/img/visit1.jpg"
     },
     {
         id: 2,
-        image: "http://localhost/BlueValley/img/visit2.jpg",
+        image: "http://localhost/BlueValley/img/visit2.jpg"
     },
     {
         id: 3,
-        image: "http://localhost/BlueValley/img/visit3.jpg",
+        image: "http://localhost/BlueValley/img/visit3.jpg"
     },
     {
         id: 4,
-        image: "http://localhost/BlueValley/img/visit4.jpg",
+        image: "http://localhost/BlueValley/img/visit4.jpg"
     }
-]
+];
 
 const brand = [{
         id: 1,
         title: "Hotel Lotus",
-        image: "http://localhost/BlueValley/img/lotus.jpg",
+        image: "http://localhost/BlueValley/img/lotus.jpg"
     },
     {
         id: 2,
         title: "Hotel Sea",
-        image: "http://localhost/BlueValley/img/sea.jpg",
+        image: "http://localhost/BlueValley/img/sea.jpg"
     },
     {
         id: 3,
         title: "Hotel Paras",
-        image: "http://localhost/BlueValley/img/paras.jpg",
+        image: "http://localhost/BlueValley/img/paras.jpg"
     }
-]
+];
 
-const myvisit = document.querySelector(".myvisit")
+const myvisit = document.querySelector(".myvisit");
 
 window.addEventListener("DOMContentLoaded", function() {
     const showInHtml = vist.map((visititem, index) => {
@@ -47,11 +47,11 @@ window.addEventListener("DOMContentLoaded", function() {
             </div>
         </div>              
     `
-    })
+    }).join('')
 
     myvisit.innerHTML = showInHtml
 })
-const mybrand = document.querySelector(".mybrand")
+const mybrand = document.querySelector(".mybrand");
 
 window.addEventListener("DOMContentLoaded", function() {
     const showInHtml = brand.map((branditem, index) => {
